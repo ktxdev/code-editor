063f45b83b43b3e9556d804b749fab8de7df9f6b
ktxdev <sean.ktxdev@gmail.com>
2022-06-22 18:45:25 +0200

    Write tests for python and javascript

--- src/test/java/com/ktxdev/codeeditor/service/CodeExecutorServiceUnitTest.java
+++ src/test/java/com/ktxdev/codeeditor/service/CodeExecutorServiceUnitTest.java
@@ -7,10 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
@@ -38,4 +36,62 @@ class CodeExecutorServiceUnitTest {
         assertTrue(result.isSuccess());
     }
 
+    @Test
+    public void whenGivenInvalidJavaCode_thenShouldNotExecuteSuccessfully() {
+        String code = "public class Solution {\n" +
+                "\tpublic static void main(String[] args) {\n" +
+                "\t\tSystem.out.println(\"Hello world! Just testing\")\n" +
+                "\t}\n" +
+                "}";
+
+        val request = new CodeExecutionRequest(Language.JAVA, code, "HelloWorld.java");
+
+        val result = codeExecutorService.execute(request);
+        log.info("### Code executed result: {}", result);
+        assertFalse(result.isSuccess());
+    }
+
+    @Test
+    public void whenGivenValidPythonCode_thenShouldExecuteSuccessfully() {
+        String code = "print(\"Hello from Python\")";
+
+        val request = new CodeExecutionRequest(Language.PYTHON3, code, "hello.py");
+
+        val result = codeExecutorService.execute(request);
+        log.info("### Code executed result: {}", result);
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    public void whenGivenInValidPythonCode_thenShouldNotExecuteSuccessfully() {
+        String code = "print(\"Hello from Python\"";
+
+        val request = new CodeExecutionRequest(Language.PYTHON3, code, "hello.py");
+
+        val result = codeExecutorService.execute(request);
+        log.info("### Code executed result: {}", result);
+        assertFalse(result.isSuccess());
+    }
+
+    @Test
+    public void whenGivenValidJavaScriptCode_thenShouldExecuteSuccessfully() {
+        String code = "console.log(\"Hello from JavaScript\")";
+
+        val request = new CodeExecutionRequest(Language.JAVASCRIPT, code, "script.js");
+
+        val result = codeExecutorService.execute(request);
+        log.info("### Code executed result: {}", result);
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    public void whenGivenValidJavaScriptCode_thenShouldNotExecuteSuccessfully() {
+        String code = "cons.log(\"Hello from JavaScript\")";
+
+        val request = new CodeExecutionRequest(Language.JAVASCRIPT, code, "script.js");
+
+        val result = codeExecutorService.execute(request);
+        log.info("### Code executed result: {}", result);
+        assertFalse(result.isSuccess());
+    }
 }
